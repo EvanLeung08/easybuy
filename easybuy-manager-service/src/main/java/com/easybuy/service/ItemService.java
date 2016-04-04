67fb718ad5235831d089069d463fd068626a3eca
10856214 <10856214@163.com>
2016-04-05 07:39:59 +0800

    更新了插入商品逻辑

--- easybuy-manager-service/src/main/java/com/easybuy/service/ItemService.java
+++ easybuy-manager-service/src/main/java/com/easybuy/service/ItemService.java
@@ -1,10 +1,12 @@
 package com.easybuy.service;
 
 import com.easybuy.pojo.EasyUIDataGridResult;
+import com.easybuy.pojo.EasybuyResult;
 import com.easybuy.pojo.TbItem;
 
 public interface ItemService {
 
 	public TbItem getItemById(Long itemId);
 	public EasyUIDataGridResult getItemList(int page, int rows);
+	public EasybuyResult createItem(TbItem item ,String desc);
 }
--- easybuy-manager-service/src/main/java/com/easybuy/service/impl/ItemServiceImpl.java
+++ easybuy-manager-service/src/main/java/com/easybuy/service/impl/ItemServiceImpl.java
@@ -1,16 +1,21 @@
 package com.easybuy.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.easybuy.mapper.TbItemDescMapper;
 import com.easybuy.mapper.TbItemMapper;
 import com.easybuy.pojo.EasyUIDataGridResult;
+import com.easybuy.pojo.EasybuyResult;
 import com.easybuy.pojo.TbItem;
+import com.easybuy.pojo.TbItemDesc;
 import com.easybuy.pojo.TbItemExample;
 import com.easybuy.pojo.TbItemExample.Criteria;
 import com.easybuy.service.ItemService;
+import com.easybuy.utils.IDUtils;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
@@ -18,7 +23,8 @@ import com.github.pagehelper.PageInfo;
 public class ItemServiceImpl implements ItemService {
 	@Autowired
 	private TbItemMapper tbItemMapper;
-
+	@Autowired
+	private TbItemDescMapper tbItemDescMapper;
 	@Override
 	public TbItem getItemById(Long itemId) {
 		TbItemExample example = new TbItemExample();
@@ -49,6 +55,43 @@ public class ItemServiceImpl implements ItemService {
 		return result;
 	}
 
+	/**
+	 * 创建商品
+	 * <p>Title: createItem</p>
+	 * <p>Description: </p>
+	 * @param item
+	 * @param desc
+	 * @return
+	 * @see com.easybuy.service.ItemService#createItem(com.easybuy.pojo.TbItem, java.lang.String)
+	 */
+	@Override
+	public EasybuyResult createItem(TbItem item, String desc) {
+		//生成商品ID
+		long itemId = IDUtils.genItemId();
+		//填充商品属性
+		item.setId(itemId);
+		//商品状态 '商品状态，1-正常，2-下架，3-删除'
+		item.setStatus((byte) 1);
+		//创建时间和更新时间
+		Date curDate = new  Date();
+		item.setCreated(curDate);
+		item.setUpdated(curDate);
+		//插入商品信息
+		tbItemMapper.insert(item);
+		//维护商品描述属性
+		 TbItemDesc itemDesc = new TbItemDesc();
+		 itemDesc.setItemId(itemId);
+		 itemDesc.setItemDesc(desc);
+		 itemDesc.setCreated(curDate);
+		 itemDesc.setUpdated(curDate);
+		 //插入数据到商品描述表中
+		 tbItemDescMapper.insert(itemDesc);
+		 
+		 return EasybuyResult.ok();
+	}
+
+	
+	
 	
 	
 
